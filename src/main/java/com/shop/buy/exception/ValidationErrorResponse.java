5f83bb7d9e12b749e3ed2665c39860ef7a202d24
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-05-14 20:27:02 -0300

    Implement custom exception handling with DuplicateResourceException and ResourceNotFoundException; add GlobalExceptionHandler and ErrorResponse classes.

---
+++ src/main/java/com/shop/buy/exception/ValidationErrorResponse.java
@@ -0,0 +1,24 @@
+package com.shop.buy.exception;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class ValidationErrorResponse extends ErrorResponse {
+    private Map<String, String> errors;
+
+    public ValidationErrorResponse() {
+    }
+
+    public ValidationErrorResponse(int status, String message, LocalDateTime timestamp, String path, Map<String, String> errors) {
+        super(status, message, timestamp, path);
+        this.errors = errors;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+}
