6e23d9e03fbd8a3a0c83f77433fe9f7c24143eae
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-05-19 21:14:19 -0300

    fix query syntax for brand name search in BrandRepository

--- src/main/java/com/shop/buy/repository/BrandRepository.java
+++ src/main/java/com/shop/buy/repository/BrandRepository.java
@@ -14,19 +14,19 @@ public interface BrandRepository extends JpaRepository<Brand, Long> {
 
     @Query(value = "SELECT * FROM brands", nativeQuery = true)
     List<Brand> findAllBrands();
-    
+
     @Query(value = "SELECT * FROM brands WHERE id = :id", nativeQuery = true)
     Optional<Brand> findBrandById(@Param("id") Long id);
-    
-    @Query(value = "SELECT * FROM brands WHERE name LIKE %:name%", nativeQuery = true)
+
+    @Query(value = "SELECT * FROM brands WHERE name LIKE CONCAT('%', :name, '%')", nativeQuery = true)
     List<Brand> findBrandsByNameContaining(@Param("name") String name);
-    
+
     @Query(value = "INSERT INTO brands (name, country, description) VALUES (:#{#brand.name}, :#{#brand.country}, :#{#brand.description}) RETURNING *", nativeQuery = true)
     Brand saveBrand(@Param("brand") Brand brand);
-    
+
     @Query(value = "UPDATE brands SET name = :#{#brand.name}, country = :#{#brand.country}, description = :#{#brand.description} WHERE id = :id RETURNING *", nativeQuery = true)
     Brand updateBrand(@Param("id") Long id, @Param("brand") Brand brand);
-    
+
     @Query(value = "DELETE FROM brands WHERE id = :id", nativeQuery = true)
     @org.springframework.data.jpa.repository.Modifying
     void deleteBrand(@Param("id") Long id);
