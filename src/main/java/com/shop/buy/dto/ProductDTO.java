576a4cf46d5f6f7f16d5e713f84c771ab28a57b7
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-06-02 19:48:45 -0300

    feat: send the complete entity in Product get requests

--- src/main/java/com/shop/buy/dto/ProductDTO.java
+++ src/main/java/com/shop/buy/dto/ProductDTO.java
@@ -5,6 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
+
+import com.shop.buy.model.Brand;
+import com.shop.buy.model.Category;
+import com.shop.buy.model.Supplier;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,11 +33,20 @@ public class ProductDTO {
   @Positive(message = "Preço deve ser um valor positivo")
   private BigDecimal price;
 
+  @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+  private Category category;
+ 
   @NotNull(message = "Categoria é obrigatória")
   private Long categoryId;
 
+  @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+  private Brand brand;
+
   @NotNull(message = "Marca é obrigatória")
   private Long brandId;
+
+  @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+  private Supplier supplier;
 
   @NotNull(message = "Fornecedor é obrigatório")
   private Long supplierId;
